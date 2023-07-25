85d142488dcb1d15db9f142373e7b58dd7f41415
Derit7042 <junsu6752@gmail.com>
2023-07-26 02:36:45 +0900

    2023_07_26_03

--- src/Chapter1/Main.java
+++ src/Chapter1/Main.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a*(b%10));
-        System.out.println(a*((b/10)%10));
-        System.out.println(a*(b/100));
-        System.out.println(a*b);
+        long a;
+        long b;
+        long c;
+
+        a = sc.nextLong();
+        b = sc.nextLong();
+        c = sc.nextLong();
+        System.out.println(a + b + c);
     }
 }
