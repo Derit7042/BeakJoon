3cf94441cd15b5b13e3aab5d9df9cf2f54735993
Derit7042 <junsu6752@gmail.com>
2023-07-26 02:39:46 +0900

    2023_07_26_04

--- src/Chapter1/Main.java
+++ src/Chapter1/Main.java
@@ -3,14 +3,10 @@ package Chapter1;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a;
-        long b;
-        long c;
-
-        a = sc.nextLong();
-        b = sc.nextLong();
-        c = sc.nextLong();
-        System.out.println(a + b + c);
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
     }
 }
