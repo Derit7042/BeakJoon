a1f532bd74c01524fe06a38328f7da9963136046
Derit7042 <junsu6752@gmail.com>
2023-12-08 22:52:48 +0900

    2023_12_08_1 두 수 비교하기

---
+++ src/Chapter2/chap2_01/Main.java
@@ -0,0 +1,22 @@
+package Chapter2.chap2_01;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if ( A == B) {
+            System.out.println("==");
+        } else {
+            System.out.println("<");
+        }
+    }
+
+}
---
+++ src/Chapter2/chap2_02/Main.java
@@ -0,0 +1,4 @@
+package Chapter2.chap2_02;
+
+public class Main {
+}
