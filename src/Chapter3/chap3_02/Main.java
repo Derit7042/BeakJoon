b57c669c8bd154e543b9ccb582a7f18763f859b8
Derit7042 <junsu6752@gmail.com>
2023-12-09 14:26:21 +0900

    2023_12_09_4 ch03_02 A+B - 3

---
+++ src/Chapter3/chap3_02/Main.java
@@ -0,0 +1,16 @@
+package Chapter3.chap3_02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            System.out.println(b+c);
+        }
+    }
+}
