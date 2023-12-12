c0139a0b0f85dfa50652b00163e7b0e8572004c9
Derit7042 <junsu6752@gmail.com>
2023-12-12 22:47:24 +0900

    2023_12_12_1 ch03_09 별 찍기 - 1

---
+++ src/Chapter3/chap3_09/Main.java
@@ -0,0 +1,18 @@
+package Chapter3.chap3_09;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int count = 0;
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
